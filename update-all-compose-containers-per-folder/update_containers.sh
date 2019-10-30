e3ba05f8ed5e573a1d4babc1430769b5659f87ec
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2019-10-30 15:17:54 +0100

    Update update_containers.sh

--- update-all-compose-containers-per-folder/update_containers.sh
+++ update-all-compose-containers-per-folder/update_containers.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+function print_line_break() {
+  printf '%20s\n' | tr ' ' -
+}
+
 readarray -d '' composeConfigs < <(find . -type f -name docker-compose.y* -print0)
 
 for cfg in "${composeConfigs[@]}"; do
+  echo ""
+  echo "[Updating docker-compose containers for config: $cfg]"
+  print_line_break
   docker-compose -f "$cfg" pull
   docker-compose -f "$cfg" up -d
 done
